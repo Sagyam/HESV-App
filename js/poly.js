cf127eb37b14495ce8436948340f4428ecb1b97c
Sagyam Thapa <sagyamthapa32@gmail.com>
2024-01-08 11:49:48 +0545

    Update backedn URL

--- js/poly.js
+++ js/poly.js
@@ -1,8 +1,8 @@
 const polyDetectUrl =
-	"https://hesv-api.up.railway.app/equations/get-polynomial-equation";
+	"https://hesv-api.onrender.com/equations/get-polynomial-equation";
 
 const polySolveUrl =
-	"https://hesv-api.up.railway.app/equations/solve-polynomial-equation";
+	"https://hesv-api.onrender.com/equations/solve-polynomial-equation";
 
 let elt = document.getElementById("calculator");
 let calculator = Desmos.GraphingCalculator(
